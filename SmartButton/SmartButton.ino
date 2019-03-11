aa4992ed8629b88bdfa9684ad8b820c08c307a0b
sahilyadav152 <sahilyadav_dwarika@srmuniv.edu.in>
2019-03-11 16:22:43 +0530

    Added LCD

--- SmartButton/SmartButton.ino
+++ SmartButton/SmartButton.ino
@@ -1,7 +1,12 @@
 #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
 SoftwareSerial mySerial(3,4);
+const int rs = 12, en = 11, d4 = 9, d5 = 8, d6 = 7, d7 = 6;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int  buttonPin = 2;
+const int lcdDisplayPin = A0;
+const int lcdContrastPin = 10;
 
 int buttonPushCounter = 0;
 int buttonState = 0;     
@@ -10,6 +15,10 @@ int lastButtonState = 0;
 void setup()
 {
   pinMode(buttonPin, INPUT);
+  pinMode(lcdDisplayPin, OUTPUT);
+  pinMode(lcdContrastPin, OUTPUT);
+  lcd.begin(16, 2);
+  analogWrite(lcdContrastPin, 20);
   mySerial.begin(9600);
   Serial.begin(9600);
   Serial.println("GSM SIM900A BEGIN");
@@ -30,9 +39,20 @@ void loop()
       buttonPushCounter++;
       Serial.print("number of button pushes: ");
       Serial.println(buttonPushCounter);
+      digitalWrite(lcdDisplayPin, HIGH);
+      lcd.display();
+      lcd.clear();
+      lcd.print("Sending");
+      lcd.setCursor(0, 1);
+      lcd.print("complaint...");
       MakeCall();
       delay(25000);
       HangupCall();
+      lcd.clear();
+      lcd.print("Complaint Sent");
+      delay(5000);
+      lcd.noDisplay();
+      digitalWrite(lcdDisplayPin, LOW);
     }
     delay(50);
   }
