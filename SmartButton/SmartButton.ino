7923de5b8270022ba194e0d12ebd206ac87628db
harshithmohan <gharshitmohan@ymail.com>
2019-03-11 21:44:07 +0530

    Long press for acknowledgement

--- SmartButton/SmartButton.ino
+++ SmartButton/SmartButton.ino
@@ -12,6 +12,12 @@ int buttonPushCounter = 0;
 int buttonState = 0;     
 int lastButtonState = 0;
 
+long millis_held;
+long secs_held;
+long prev_secs_held;
+byte previous = LOW;
+unsigned long firstTime;
+
 void setup()
 {
   pinMode(buttonPin, INPUT);
@@ -23,8 +29,8 @@ void setup()
   Serial.begin(9600);
   Serial.println("GSM SIM900A BEGIN");
   Serial.println("Enter character for control option:");
-  Serial.println("1 press : to make a call for complaint registration");
-  Serial.println("3 presses : to send acknowledgement message");
+  Serial.println("Single Press : to make a call for complaint registration");
+  Serial.println("Long Press : to send acknowledgement message");
   Serial.println();
   delay(100);
 }
@@ -38,32 +44,67 @@ void loop()
     if (buttonState == HIGH) {
       buttonPushCounter++;
       Serial.print("number of button pushes: ");
-      Serial.println(buttonPushCounter);
+      Serial.println(buttonPushCounter);      
+    }
+    delay(50);
+  }
+
+  if (buttonState == HIGH && lastButtonState == LOW && (millis() - firstTime) > 200) {
+    firstTime = millis();
+  }
+
+  millis_held = (millis() - firstTime);
+  secs_held = millis_held / 1000;
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    if (secs_held <= 1) {
+        digitalWrite(lcdDisplayPin, HIGH);
+        lcd.display();
+        lcd.clear();
+        SendComplaint();
+        Serial.println("Complaint Sent");
+        lcd.clear();
+        lcd.print("Complaint Sent");
+        delay(5000);
+        lcd.noDisplay();
+        digitalWrite(lcdDisplayPin, LOW);
+      }
+
+    if (secs_held >= 3 && buttonPushCounter>1) {
       digitalWrite(lcdDisplayPin, HIGH);
       lcd.display();
       lcd.clear();
       lcd.print("Sending");
       lcd.setCursor(0, 1);
-      lcd.print("complaint...");
-      MakeCall();
-      delay(25000);
-      HangupCall();
+      lcd.print("acknowledgement...");
+      SendAck();
+      delay(5000);
+      //MakeAckCall();
+      //delay(25000);
+      //HangupCall();
+      Serial.println("Acknowledgement Sent");
       lcd.clear();
-      lcd.print("Complaint Sent");
+      lcd.print("Acknowledgement Sent");
       delay(5000);
       lcd.noDisplay();
       digitalWrite(lcdDisplayPin, LOW);
     }
-    delay(50);
-  }
-  lastButtonState = buttonState;
+    }
+    lastButtonState = buttonState;
+    prev_secs_held = secs_held;
 }
 
 
 void MakeCall()
 {
-  mySerial.println("ATD+919536076959;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
-  Serial.println("Calling  "); // print response over serial port
+  mySerial.println("ATD+918320841726;");
+  Serial.println("Calling  ");
+  delay(5000);
+}
+
+void MakeAckCall()
+{
+  mySerial.println("ATD+919536076959;");
+  Serial.println("Calling  ");
   delay(5000);
 }
 
@@ -72,4 +113,30 @@ void HangupCall()
   mySerial.println("ATH");
   Serial.println("Hangup Call");
   delay(1000);
+}
+
+void SendAck()
+{
+  Serial.println("Sending acknowledgement\n");
+  mySerial.println("AT+CMGF=1");
+  delay(2000);
+  mySerial.println("AT+CMGS=\"9536076959\"\r");
+  delay(2000);
+  mySerial.println("AC of SIIC meeting room has been fixed\n");
+  delay(2000);
+  mySerial.println((char)26);
+  delay(2000);
+}
+
+void SendComplaint()
+{
+  Serial.println("Sending message\n");
+  mySerial.println("AT+CMGF=1");
+  delay(2000);
+  mySerial.println("AT+CMGS=\"8320841726\"\r");
+  delay(2000);
+  mySerial.println("AC of SIIC meeting room is not working properly.\nPlease register the complaint.\nADDRESS:SIIC LABS, 5TH floor, BEL LAB, SRM UNIVERSITY\n");
+  delay(2000);
+  mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(2000);
 }
